363af3cce6e919a597ceef55e9d648398eaa02c6
miguel-molia <miguel.molia@laplateforme.io>
2023-04-28 13:33:34 +0200

    git all ok

--- index.php
+++ index.php
@@ -18,6 +18,10 @@ require("./bdd.php");
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 
 
 </head>
@@ -55,7 +59,14 @@ require("./bdd.php");
 
     <main>
 
-
+    <p>
+        
+    <div class="textom"> Bienvenue tous le monde! <br>   
+        Voici la liste officielle des 22 joueurs de <span class="om">l'Olympique de Marseille</span> <br>pour la saison 2022/2023!<br>
+    <span class="allezom">ALLEZ L'OM!</span>
+    </div>
+    
+    </p>
 
 
 
